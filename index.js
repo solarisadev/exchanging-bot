6fbe0aa204403514dad543e0bba56d7812aedc84
Damon French <155218306+solarisadev@users.noreply.github.com>
2025-02-20 16:48:45 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { loadCommands } = require('./loadCommands');
 const { loadEvents } = require('./loadEvents');
-const config = require('./config.json');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('Connected to MongoDB'))
+        .catch(err => console.error('Failed to connect to MongoDB', err));
 });
 
 loadCommands(client);
